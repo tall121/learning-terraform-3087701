1143fd3d37ef9176a2b2f8ae59a0b564fb27f647
tall121 <karvin19@hotmail.com>
2024-05-15 09:54:38 +0100

    vpc

--- main.tf
+++ main.tf
@@ -13,12 +13,43 @@ data "aws_ami" "app_ami" {
 
   owners = ["979382823631"] # Bitnami
 }
+module "blog_vpc" {
+  source = "terraform-aws-modules/vpc/aws"
 
+  name = "Dev"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["eu-west-1a", "eu-west-1b", "eu-west-1c"]
+  #private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  #enable_nat_gateway = true
+  #enable_vpn_gateway = true
+
+  tags = {
+    Terraform = "true"
+    Environment = "dev"
+  }
+}
 resource "aws_instance" "web" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
-
+  vpc_security_group_ids=[module.blog_sg.vpc_security_group_id]
+subnet_id=module.blog_vpc.public_subnets[0]
   tags = {
     Name = "HelloWorld"
   }
+}
+
+module "blog_sg_sg" {
+  source = "terraform-aws-modules/security-group/aws"
+
+  name        = "blog-service"
+  description = "Security group for user-service with custom ports open within VPC, and PostgreSQL publicly open"
+  vpc_id      = module.blog_vpc.vpc_id
+  
+  ingress_cidr_blocks      = ["0.0.0.0/0"]
+  ingress_rules            = ["https-443-tcp","https-80-tcp"]
+  egress_cidr_blocks      = ["0.0.0.0/0"]
+  egress_rules            = ["all-all"]
 }
