1fb5198b0f40c5eee27942636b5cb1dba683f993
tall121 <karvin19@hotmail.com>
2024-05-17 08:33:42 +0100

    blog_sg

--- main.tf
+++ main.tf
@@ -47,7 +47,7 @@ module "blog_alb" {
   name    = "blog-alb"
   vpc_id  = module.blog_vpc.vpc_id
   subnets = module.blog_vpc.public_subnets
-  security_groups =module.sg.security_group_id
+  security_groups =module.blog_sg.security_group_id
 
   access_logs = {
     bucket = "blog-alb-logs"
